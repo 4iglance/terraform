96b95fb7f02917c66b4606e5d9c68ec2dee59de7
israrulhaque-at-847970100731 <hisrarul@gmail.com>
2019-09-24 12:04:18 +0530

    added vpc module

---
+++ modules/vpc/routetable.tf
@@ -0,0 +1,19 @@
+resource "aws_route_table" "pub-RT" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "publicRT-devops-terraform"
+  }
+}
+
+# Attach route table with public subnet
+resource "aws_route_table_association" "RT-Assoc" {
+  count = "${length(var.public_subnet)}"
+  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+  route_table_id = "${aws_route_table.pub-RT.id}"
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "public_subnet" {
+  type = list(string)
+  default = ["20.0.0.0/18", "20.0.64.0/18"]
+}
+
+variable "private_subnet" {
+  type = list(string)
+  default = ["20.0.128.0/18", "20.0.192.0/18"]
+}
+
+variable "vpc_cidr" {
+  default = "20.0.0.0/16"
+}
+
